49972d6b9b12a944763fa8a445ab3b163b827f48
Steven Frieson <sfrieson@gmail.com>
2016-02-17 18:52:08 -0500

    Adds portoflio items and styling to go with it.

--- js/test.js
+++
@@ -1,5 +0,0 @@
-var newName = function(name){
-    console.log("Here is a new", name);
-};
-
-//oh yeah
--- js/test1.js
+++
@@ -1,4 +0,0 @@
-var newGame = function(game){
-    console.log("Here's the new", game);
-};
-console.log("The JS seems to be working, but not the SASS");
